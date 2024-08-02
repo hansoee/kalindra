023ec23b99ace160e496063ca522c8f9ef97741c
hannarchmsr <hannarchmsr@gmail.com>
2024-08-02 16:07:07 +0700

    add trackpoints and threejs

---
+++ components/Three.tsx
@@ -0,0 +1,18 @@
+import React, { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+
+const ThreeScene: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      containerRef.current?.appendChild(renderer.domElement);
+      camera.position.z = 5;
+    }
+  }, []);
+  return <div ref={containerRef} />;
+};
+export default ThreeScene;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import dynamic from "next/dynamic";
 import { LatLngTuple } from 'leaflet';
+import Head from 'next/head';
+import Three from '../components/Three'
 
 const Home: React.FC = () => {
   const Map = dynamic(() => import('../components/Map'), {
@@ -8,17 +10,20 @@ const Home: React.FC = () => {
 
   const positions: LatLngTuple[] = [
     [-7.80166, 112.51804],
-    [-24.63744117850105, 113.24092548920918],
-    [-24.634748305662095, 113.23909861990798],
-    [-24.633230409962335, 113.23862018694673],
-    [-24.632889321076148, 113.2392011329192],
-    [-24.630923805470143, 113.23785731138456],
-    [-24.631396217618788, 113.23688869490068],
+    [-7.808751, 112.517536],
+    [-7.805427, 112.515759],
+    [-7.803838, 112.515297],
+    [-7.803475, 112.515869],
+    [-7.801844, 112.513771],
   ];
 
   return (
     <div>
+      <Head>
+        <script src="https://threejs.org/build/three.js"></script>
+      </Head>
       <Map positions={positions} zoom={17} />
+      <Three/>
     </div>
   );
 };
