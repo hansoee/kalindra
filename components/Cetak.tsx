2e797c34062ad5141f4a3d6c99463ab346c91b48
hannarchmsr <hannarchmsr@gmail.com>
2024-08-06 15:59:32 +0700

    add pdf reader and gearth

--- components/Cetak.tsx
+++ components/Cetak.tsx
@@ -1,24 +1,29 @@
-// cetak.tsx
 import { useEffect, useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
-// Set workerSrc to the worker provided by pdfjs-dist
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+// Set workerSrc to the local worker file provided by pdfjs-dist
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.1.81/pdf.worker.min.js`;
 
-export default function Cetak(){
+const Cetak: React.FC = () => {
   const [numPages, setNumPages] = useState<number | null>(null);
   const [pageNumber, setPageNumber] = useState(1);
 
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
+    console.log('PDF loaded successfully');
+  };
+
+  const onDocumentLoadError = (error: Error) => {
+    console.error('Error while loading PDF:', error);
   };
 
   return (
     <div style={{ width: '100%', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
       <Document
-        file="./petaCetak.pdf" 
+        file="https://drive.google.com/file/d/11X1aPoTDtkTzNukbcmzEIEmNecOxVkiq/view?usp=sharing"
         onLoadSuccess={onDocumentLoadSuccess}
+        onLoadError={onDocumentLoadError}
       >
         <Page pageNumber={pageNumber} />
       </Document>
@@ -36,3 +41,5 @@ export default function Cetak(){
     </div>
   );
 };
+
+export default Cetak;
